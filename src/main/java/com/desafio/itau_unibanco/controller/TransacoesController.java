9d2ef2404af46524169139d1ccf781b439eeb7c1
Felix Eduardo Villanueva Condori <felixeduardobx@gmail.com>
2025-07-10 11:09:47 -0300

    Base POST 1.2 (Validações adicionadas)

--- src/main/java/com/desafio/itau_unibanco/controller/TransacoesController.java
+++ src/main/java/com/desafio/itau_unibanco/controller/TransacoesController.java
@@ -5,12 +5,27 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
 @RestController
 @RequestMapping("/transacao")
 public class TransacoesController {
 
     @PostMapping
-    public ResponseEntity<Void> efetuarTransacao(@RequestBody Transacao dadosTransacao){
+    public ResponseEntity<Void> efetuarTransacao(@RequestBody Transacao transacao){
+        if(transacao.getValor() == null || transacao.getDataHora() == null){
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
+        if(transacao.getDataHora().isAfter(OffsetDateTime.now())){
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
+        if(transacao.getValor().compareTo(BigDecimal.ZERO) < 0){
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
