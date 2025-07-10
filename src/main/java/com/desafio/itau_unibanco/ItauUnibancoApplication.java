f2217edc3e4ad40c0a9aaaf3f3fdeb77fe5922bc
Felix Eduardo Villanueva Condori <felixeduardobx@gmail.com>
2025-07-10 09:41:18 -0300

    Ambiente preparado

---
+++ src/main/java/com/desafio/itau_unibanco/ItauUnibancoApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.itau_unibanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauUnibancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauUnibancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/itau_unibanco/ItauUnibancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.itau_unibanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauUnibancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
